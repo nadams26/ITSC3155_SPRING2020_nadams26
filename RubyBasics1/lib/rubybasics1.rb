d26e04daeed5ee0638396cc6a33fbb2ff6176cb1
Nathan Adams <nadams26@uncc.edu>
2020-01-28 18:58:00 +0000

    RubyBasics1 - Part 1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,19 @@
+# Lab 1
+# Part I
+def sum arr
+  sum_var = 0
+  arr.each do |x|
+    sum_var += x
+  end
+  return sum_var
+end
+
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
