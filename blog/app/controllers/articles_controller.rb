9342c4f56c0de3bd1f80eb3ce100457c96147352
Nathan Adams <nadams26@uncc.edu>
2020-03-31 17:38:44 +0000

    Add Edit/Update functionality

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -8,7 +8,18 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
-    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
     def create
         @article = Article.new(article_params)
         if @article.save
